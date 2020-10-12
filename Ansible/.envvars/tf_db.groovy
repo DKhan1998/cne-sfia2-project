6034414416eb4dce2bf022456dc05b2dbaaa4049
Daood Khan <dkhan@qa.com>
2020-10-12 13:22:51 +0100

    working on jenkinsfiule to pass terraform env variables
    Signed-off-by: Daood Khan <DKhan@qa.com>

--- Ansible/.envvars/tf_db.groovy
+++ Ansible/.envvars/tf_db.groovy
@@ -3,5 +3,6 @@ env.TEST_DATABASE_URI="mysql+pymysql://admin:password@testdb.cgytirb7uezx.eu-wes
 env.DATABASE_URI="mysql+pymysql://admin:password@deploydb.cgytirb7uezx.eu-west-2.rds.amazonaws.com:3306:3306/users"
 env.SECRET_KEY="password"
 env.MYSQL_ROOT_PASSWORD="password"
+env.DB_PASSWORD="password"
 env.testvm_connection="mysql -h testdb.cgytirb7uezx.eu-west-2.rds.amazonaws.com:3306 -P 3306 -u admin -ppassword"
 
