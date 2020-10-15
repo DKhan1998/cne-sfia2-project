3663613075351e8b863cc5e2555087fe80c4c802
Daood Khan <dkhan@qa.com>
2020-10-15 10:47:28 +0100

    Project is complete, all hsould work fine. html document does not change
    Signed-off-by: Daood Khan <DKhan@qa.com>

--- Ansible/.envvars/tf_db.groovy
+++
@@ -1,8 +0,0 @@
-// project variables to pass for databse
-env.TEST_DATABASE_URI="mysql+pymysql://admin:password@test-db.cgytirb7uezx.eu-west-2.rds.amazonaws.com:3306/testdb"
-env.DATABASE_URI="mysql+pymysql://admin:password@deploy-db.cgytirb7uezx.eu-west-2.rds.amazonaws.com:3306/users"
-env.SECRET_KEY="password"
-env.MYSQL_ROOT_PASSWORD="password"
-env.DB_PASSWORD="password"
-env.testvm_connection="mysql -h test-db.cgytirb7uezx.eu-west-2.rds.amazonaws.com:3306 -P 3306 -u admin -ppassword"
-
